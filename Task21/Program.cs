3d37a83de54ab06c8dc4f84176e27af651953475
Artem Voytenko <bobinrobin12@gmail.com>
2023-01-23 20:37:01 +0300

    Создали второй проект по дз

---
+++ Task21/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+WriteLine("Введите координату Х первой точки: ");
+int x1 = int.Parse(ReadLine());
+WriteLine("Введите координату Y первой точки: ");
+int y1 = int.Parse(ReadLine());
+WriteLine("Введите координату Z первой точки: ");
+int z1 = int.Parse(ReadLine());
+WriteLine("Введите координату Х второй точки: ");
+int x2 = int.Parse(ReadLine());
+WriteLine("Введите координату Y второй точки: ");
+int y2 = int.Parse(ReadLine());
+WriteLine("Введите координату Z второй точки: ");
+int z2 = int.Parse(ReadLine());
