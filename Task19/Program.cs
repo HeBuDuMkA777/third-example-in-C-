ec497ee846fe991f72e682b2745ac58261fe37e2
Artem Voytenko <bobinrobin12@gmail.com>
2023-01-23 20:31:38 +0300

    выполнил задание!

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Введите число: ");
+int num = int.Parse(ReadLine());
+int first = num % 10;
+int sec = (num % 100) / 10;
+int thir = (num % 1000) / 100;
+int four = (num % 10000) / 1000;
+int five = num / 10000;
+int palind = (first * 10000) + (sec * 1000) + (thir * 100) + (four * 10) + five;
+
+if (palind == num)
+{
+    WriteLine("Число палиндром!");
+}
+else
+{
+    WriteLine("Нет, число не палиндром!");
+}
